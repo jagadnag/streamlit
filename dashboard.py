eea62eb21d24b1b6088ac4dd7c75d60972246d7d
Jag <jagadnag@gmail.com>
2021-09-26 15:37:28 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,16 +4,16 @@ import pandas as pd
 
 st.title("Stock Tracking Dashboard")
 
-option = st.sidebar.selectbox("Which Dashboard?", ('FANG', 'INDEX', 'IBD', 'Master', 'SPDR', 'CP'))
+option = st.sidebar.selectbox("Which Dashboard?", ('FANG', 'INDEX', 'IBD', 'Master', 'SPDR'))
 
 timing = st.sidebar.selectbox("What Timeframe?", ('Daily', 'Weekly', 'Both'))
 
 st.header(option)
 
 if option == "FANG":
-    symbols=['QQQ', 'AAPL', 'AMZN', 'MSFT', 'GOOGL', 'FB', 'NFLX', 'NVDA', 'TSLA', 'ADBE', 'PYPL', 'SQ', 'SE', 'ROKU', 'SHOP', 'TWLO', 'DOCU', 'ZM', 'ZS', 'NOW',
-             'CRWD', 'NET', 'FTNT', 'TTD', 'CRM', 'TEAM', 'ASML', 'LRCX', 'AMAT', 'AMD', 'TSM', 'CMG', 'COST', 'TGT', 'NKE', 'SBUX', 'HD', 'IDXX', 'INTU', 'ISRG', 'SNPS', 'UNH',
-             'DPZ', 'VEEV', 'WST', 'ACN' ]
+    symbols=['QQQ', 'AAPL', 'AMZN', 'MSFT', 'GOOGL', 'FB', 'NFLX', 'NVDA', 'TSLA', 'ADBE', 'PYPL', 'SQ', 'SE', 'ROKU', 'SHOP', 'MELI', 'TWLO', 'DOCU', 'ZM', 'ZS', 'NOW', 'TWLO', 'COUP', 'MTCH', 'SNAP',
+             'CRWD', 'NET', 'FTNT', 'TTD', 'CRM', 'TEAM', 'ASML', 'LRCX', 'AMAT', 'AMD', 'TSM', 'TXN', 'CMG', 'COST', 'TGT', 'NKE', 'LULU', 'CROX', 'SBUX', 'HD', 'IDXX', 'INTU', 'ISRG', 'SNPS', 'UNH',
+             'DPZ', 'DXCM', 'VEEV', 'WST', 'GNRC', 'AMT', 'ADI', 'TMO', 'ACN', 'HUBS', 'WDAY', 'ABNB', 'DASH', 'UBER', 'PLTR', 'SNOW', 'DDOG', 'DT']
 
     if timing == "Daily":
         for stock in symbols:
@@ -29,10 +29,10 @@ if option == "FANG":
             st.image(f"https://finviz.com/chart.ashx?t={stock}&ta=0&ty=c&p=w&s=l", width=None)
 
 if option == "INDEX":
-    symbols=['SPY', 'QQQ', 'DIA', 'IWM', 'VTI', 'VIXY', 'SQQQ', 'TQQQ', 'SSO', 'UPRO', 'SPXL', 'XLK','SMH', 'IGV', 'TAN', 'ARKK', 'ARKG', 'ARKF', 'ARKQ', 'ARKX',
+    symbols=['SPY', 'QQQ', 'DIA', 'IWM', 'VTI', 'VIXY', 'SQQQ', 'TQQQ', 'SSO', 'UPRO', 'SPXL', 'XLK','SMH', 'IGV', 'TAN', 'ARKK', 'ARKG', 'ARKF', 'ARKQ', 'ARKX', 'CIBR',
              'AAPL', 'AMZN', 'MSFT', 'FB', 'GOOGL', 'NFLX', 'NVDA', 'TSLA', 
-             'ADBE', 'PYPL', 'SQ', 'SE', 'SHOP', 'ROKU', 'DOCU', 'TWLO', 'ZM', 'ZS',
-             'LRCX', 'ASML', 'AMAT', 'AMD', 'TSM','SNAP', 'CRWD', 'NET', 'PINS', 'PTON', 'TWTR', 'NIO']
+             'ADBE', 'NOW', 'PYPL', 'SQ', 'SE', 'SHOP', 'MELI', 'ROKU', 'DOCU', 'TWLO', 'ZM', 'ZS',
+             'LRCX', 'ASML', 'AMAT', 'AMD', 'TSM','SNAP', 'CRWD', 'NET', 'PINS', 'ABNB', 'DASH', 'PLTR', 'SNOW', 'DDOG', 'UPST', 'CELH']
 
     if timing == "Daily":
         for stock in symbols:
@@ -49,7 +49,7 @@ if option == "INDEX":
 
 
 if option == "SPDR":
-    symbols=['XLK', 'XLY', 'XLP', 'XLRE', 'XLE', 'XLV', 'XLB', 'XLI', 'XLF', 'XLU', 'XLC', 'XHB']
+    symbols=['XLK', 'XLY', 'XLP', 'XLRE', 'XLE', 'XLV', 'XLB', 'XLI', 'XLF', 'XLU', 'XLC', 'XHB', 'XSW', 'XSD', 'KRE', 'XME', 'XRT','XOP', 'GLD', 'KBE', ]
 
     if timing == "Daily":
         for stock in symbols:
@@ -98,18 +98,4 @@ if option == "Master":
             st.image(f"https://finviz.com/chart.ashx?t={stock}")
             st.image(f"https://finviz.com/chart.ashx?t={stock}&ta=0&ty=c&p=w&s=l", width=None)
 
-
-if option == "CP":
-
-    d2 = st.date_input('start date', date.today())
-    #d2 = d2.strftime("%Y-%m-%d")
-    year = d2.strftime("%Y")
-    month = d2.strftime("%m")
-    day = d2.strftime("%d")
-    print(month, day)
-
-    tckr = st.text_input("Enter the symbol: ")    
-    #symbols=['XLK', 'XLY', 'XLP', 'XLRE', 'XLE', 'XLV', 'XLB', 'XLI', 'XLF', 'XLU', 'XLC', 'XHB']
-    #for stock in symbols:
-    st.image(f"http://www.chartpattern.com/charts/{tckr}-{month}-{day}-21.gif")
 

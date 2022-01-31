60472e66dc8d8e3e04d119c7f261c1911d91a1c9
Jag <jagadnag@gmail.com>
2022-01-30 22:44:52 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,16 +4,19 @@ import pandas as pd
 
 st.title("Stock Tracking Dashboard")
 
-option = st.sidebar.selectbox("Which Dashboard?", ('FANG', 'INDEX', 'IBD', 'Master', 'SPDR'))
+option = st.sidebar.selectbox("Which Dashboard?", ('FANG', 'INDEX', 'IBD', 'Master', 'DOW'))
 
 timing = st.sidebar.selectbox("What Timeframe?", ('Daily', 'Weekly', 'Both'))
 
 st.header(option)
 
 if option == "FANG":
-    symbols=['QQQ', 'AAPL', 'AMZN', 'MSFT', 'GOOGL', 'FB', 'NFLX', 'NVDA', 'TSLA', 'ADBE', 'PYPL', 'SQ', 'SE', 'ROKU', 'SHOP', 'MELI', 'TWLO', 'DOCU', 'ZM', 'ZS', 'NOW', 'TWLO', 'COUP', 'MTCH', 'SNAP',
-             'CRWD', 'NET', 'FTNT', 'TTD', 'CRM', 'TEAM', 'ASML', 'LRCX', 'AMAT', 'AMD', 'TSM', 'TXN', 'CMG', 'COST', 'TGT', 'NKE', 'LULU', 'CROX', 'SBUX', 'HD', 'IDXX', 'INTU', 'ISRG', 'SNPS', 'UNH',
-             'DPZ', 'DXCM', 'VEEV', 'WST', 'GNRC', 'AMT', 'ADI', 'TMO', 'ACN', 'HUBS', 'WDAY', 'ABNB', 'DASH', 'UBER', 'PLTR', 'SNOW', 'DDOG', 'DT']
+    symbols=['SPY', 'QQQ', 'DIA', 'IWM', 'VTI', 'VIXY', 'SQQQ', 'TQQQ', 'XLK','SMH', 'IGV', 'TAN', 'ARKK', 'FFTY', 'SSO', 'UPRO', 'QLD', 'SPXL', 'SOXL',
+             'TECL', 'TNA', 'UDOW', 'ERX', 'FAS', 'LABU', 'NUGT', 'JNUG', 'GUSH',
+             'XLK', 'XLY', 'XLP', 'XLRE', 'XLE', 'XLV', 'XLB', 'XLI', 'XLF', 'XLU', 'XLC', 'XHB', 'XSW', 'XSD', 'KRE', 'XME', 'XRT','XOP', 'GLD', 'KBE',
+             'AAPL', 'AMZN', 'MSFT', 'GOOGL', 'FB', 'NFLX', 'NVDA', 'TSLA', 'ADBE', 'NOW', 'CRM', 'TEAM', 'ASML', 'LRCX', 'AMAT', 'AMD', 'TSM', 'MU',
+             'FTNT', 'TTD',  'COST', 'HD', 'INTU', 'ISRG', 'SNPS',  'ACN', 'ABNB','SNOW', 'DDOG', 'RBLX' 'MDB', 'CRWD', 'NET', 'APPS', 'SHOP', 'ETSY',
+             'OKTA', 'ZS', 'PANW', 'ANET', 'JNPR', 'TWLO', 'ROKU', 'U', 'DOCU', 'ZM', 'UBER']
 
     if timing == "Daily":
         for stock in symbols:
@@ -29,12 +32,14 @@ if option == "FANG":
             st.image(f"https://finviz.com/chart.ashx?t={stock}&ta=0&ty=c&p=w&s=l", width=None)
 
 if option == "INDEX":
-    symbols=['SPY', 'QQQ', 'DIA', 'IWM', 'VTI', 'VIXY', 'SQQQ', 'TQQQ', 'XLK','SMH', 'IGV', 'TAN', 'ARKK', 'FFTY', 'AAPL', 'AMZN', 'MSFT', 'GOOGL', 'FB', 
-             'NFLX', 'NVDA', 'TSLA', 'AMD', 'COST', 'SHOP', 'ADBE', 'INTU', 'ACN', 'ZS','LRCX', 'ASML', 'AMAT', 'TSM', 'QCOM', 'AVGO', 'MU', 'KLAC', 'TXN',
+    symbols=['SPY', 'QQQ', 'DIA', 'IWM', 'VTI', 'VIXY', 'SQQQ', 'TQQQ', 'XLK','SMH', 'IGV', 'TAN', 'ARKK', 'FFTY', 'SSO', 'UPRO', 'QLD', 'SPXL', 'SOXL',
+             'TECL', 'TNA', 'UDOW', 'ERX', 'FAS', 'LABU', 'NUGT',  'AAPL', 'AMZN', 'MSFT', 'GOOGL', 'FB', 
+             'NFLX', 'NVDA', 'TSLA', 'AMD', 'COST', 'HD', 'LOW', 'SHOP', 'ADBE', 'INTU', 'ACN', 'ZS','LRCX', 'ASML', 'AMAT', 'TSM', 'QCOM', 'AVGO', 'MU', 'KLAC', 'TXN',
              'NOW', 'CRM', 'TEAM', 'TTD', 'PYPL', 'SQ', 'SE', 'MELI', 'ROKU', 'DOCU', 'TWLO', 'ZM', 'SPOT', 'OKTA', 'CRWD', 'NET', 'FTNT', 'PANW', 'ANET',
              'ABNB', 'DASH', 'PLTR', 'SNOW', 'DDOG', 'MDB', 'UPST', 'CELH', 'AFRM', 'ASAN', 'RBLX', 'SOFI', 'COIN', 'U', 'ASO', 'ZI', 'ZIM', 'ON', 'MRVL',
              'RIVN', 'LCID', 'HD', 'LOW', 'CROX', 'NKE', 'TGT', 'DPZ', 'LULU', 'ENPH', 'DHI', 'JPM', 'BRK.B', 'GS', 'SCHW', 'BAC', 'CAT', 'DE', 'F', 'GM',
-             'SSO', 'UPRO', 'QLD', 'SPXL', 'SOXL', 'TNA', 'UDOW', 'ERX',  'CIBR', 'FAS', 'GBTC', 'BITO', 'ETHE']
+             'XOM', 'CVN', 'DOCN', 'GLBE', 'INMD' 'APPS', 'HOOD', 'GLBE',
+              'GBTC', 'BITO', 'ETHE', 'CIBR', 'BUG', 'HACK']
 
     if timing == "Daily":
         for stock in symbols:
@@ -50,7 +55,7 @@ if option == "INDEX":
             st.image(f"https://finviz.com/chart.ashx?t={stock}&ta=0&ty=c&p=w&s=l", width=None)
 
 
-if option == "SPDR":
+if option == "DOW":
     symbols=['XLK', 'XLY', 'XLP', 'XLRE', 'XLE', 'XLV', 'XLB', 'XLI', 'XLF', 'XLU', 'XLC', 'XHB', 'XSW', 'XSD', 'KRE', 'XME', 'XRT','XOP', 'GLD', 'KBE', ]
 
     if timing == "Daily":
